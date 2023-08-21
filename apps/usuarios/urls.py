98fc35867303e99820d9e4f1e2d869d6ca45b2a3
DaromanX <48441806+developerdaro@users.noreply.github.com>
2023-08-21 12:01:16 -0500

    excel funcionando con las operaciones

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import custom_login,registro,cerrar_sesion,perfil
+from .views import custom_login,registro,cerrar_sesion,perfil,excel,inicio
 
 urlpatterns = [
     path('',custom_login,name='login'),
     path('logout/', cerrar_sesion, name='logout'),
     path('registro/',registro,name='registro'),
     path('perfil/',perfil,name='perfil'),
+    path('inicio/',inicio,name='inicio'),
+    
+    
+    
+    
+    path('excel/',excel,name='excel'),
 ]
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -17,9 +17,9 @@ def custom_login(request):
         if user:
             login(request, user)
             if user.groups.filter(name='Usuarios_Internos').exists():
-                return redirect('usuarios:perfil')  # Redirige a la página de administrador
+                return redirect('usuarios:excel')  # Redirige a la página de administrador
             elif user.groups.filter(name='Usuarios_externos').exists():
-                return redirect('usuarios:perfil')  # Redirige a la página de usuario normal
+                return redirect('usuarios:excel')  # Redirige a la página de usuario normal
         else:
             messages.error(request, 'Usuario o contraseña in    correctos.')
     return render(request, 'ingreso/login.html')
@@ -32,6 +32,24 @@ def registro(request):
     return render(request,'ingreso/registro.html')
 
 def perfil(request):
+    usuario = request.user
+
+    if request.method == 'POST' and request.FILES:
+        archivo_foto = request.FILES['nueva_foto_perfil']
+        
+        usuario.foto_perfil = archivo_foto
+        usuario.save()
+        
+        return redirect('usuarios:perfil')
+    return render(request,'usuarios/perfil.html',{'usuario':usuario})
+
+def inicio(request):
+    
+    return render(request,'usuarios/inicio.html')
+
+
+
+def excel(request):
     if request.method == 'POST' and request.FILES:
         archivo = request.FILES['archivo_excel']
         if archivo.name.endswith('.xlsx'):
@@ -41,12 +59,15 @@ def perfil(request):
             df = pd.read_excel(archivo)
             
             # Realizar la operación y crear una nueva columna 'Total'
-            df['IVA_VENTA'] = (df['Precio_venta'] * df['IVA'])/100
-            df['UTILIDAD'] = (df['Precio_venta'] * df['IVA'])/100
-            
+            df['IVA_VENTA'] = (df['Precio_venta'] * df['IVA'])/100            
+            df['UTILIDAD'] = (df['Precio_venta'] - df['Costo'])/df['Costo']
+            df['UTILIDAD']=pd.Series([round(val,2) for val in df["UTILIDAD"]])
+            df['IVA_VENTA']=pd.Series([round(val,2) for val in df["IVA_VENTA"]])
+            df = df.fillna(0)
+
             data = df.to_dict(orient='records')
             paginator = Paginator(data, 100)  # 10 elementos por página
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
-            return render(request, 'usuarios/perfil.html', {'page_obj': page_obj})
-    return render(request, 'usuarios/perfil.html')
+            return render(request, 'usuarios/excel.html', {'page_obj': page_obj})
+    return render(request, 'usuarios/excel.html')
